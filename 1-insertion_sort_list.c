45469b1e72263c5a54c3efe829a46873731e259a
JamesPagani <jaimeagalvezv@gmail.com>
2020-02-03 18:34:22 +0000

    Checked if Nothing (NULL) was passed to the algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,7 +12,11 @@ void insertion_sort_list(listint_t **list)
 	listint_t *bkup = NULL;
 	listint_t *swap = NULL;
 
-	if (list != NULL || *list != NULL || (*list)->next != NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+	else
 	{
 		node = (*list)->next;
 		while (node != NULL)
