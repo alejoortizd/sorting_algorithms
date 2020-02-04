e9544142e58597f72f220eeba5a67ff89e53284c
JamesPagani <jaimeagalvezv@gmail.com>
2020-02-04 15:19:21 +0000

    Added the quicksort algorithms prototypes

--- sort.h
+++ sort.h
@@ -21,5 +21,7 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void sub_qsort(int *array, int min, int max, size_t size);
 
 #endif
